b08132096a232ee803f91a8e9d96028f68c6b030
845860421 <millen.c.wong@gmail.com>
2016-04-21 19:56:16 +1200

    vars for input  and output messages

---
+++ js/patterns.js
@@ -0,0 +1,4 @@
+//   allowable characters for the full name
+	var firstnamePattern =  new RegExp("^[a-zA-Z' .-]{1,30}$");
+	var surnamePattern =  new RegExp("^[a-zA-Z' .-]{1,30}$");
+	var emailPattern = new RegExp("^[a-zA-Z'.-]{1,30}$");
---
+++ js/validation.js
@@ -0,0 +1,69 @@
+//Find the form
+var tourForm = document.querySelector('#book-a-tour');
+
+
+//glider flight  input vars and output message vars
+var numberGlidersInput = document.querySelector('#number-gliders');
+var numberGlidersMessage = document.querySelector('#number-gliders-message');
+
+var gliderDateInput = document.querySelector('#glider-date');
+var gliderDateMessage = document.querySelector('#glider-date-message');
+
+var gliderHoursInput = document.querySelector('#glider-hours');
+var gliderHoursMessage = document.querySelector('#glider-hours-message');
+
+
+//sightseeing flights input vars and output message vars
+var sightseeingTypeInput = document.querySelector('#sightseeing-type');
+var sightseeingTypeMessage = document.querySelector('#sightseeing-type-message');
+
+var sightseeingDateInput = document.querySelector('#sightseeing-date');
+var sightseeingDateMessage = document.querySelector('#sightseeing-date-message');
+
+var aircraftTypeInput = document.querySelector('#aircraft-type');
+var aircraftTypeMessage = document.querySelector('#aircraft-type-message');
+
+var numberSightseersInput = document.querySelector('#number-sightseers');
+var numberSightseersMessage = document.querySelector('#number-sightseers-message');
+
+
+//charters and provisiong input vars and output message vars
+var charterTypeInput = document.querySelector('#charter-type');
+var charterTypeMessage = document.querySelector('#charter-type-message');
+
+var charterDateInput = document.querySelector('#charter-date');
+var charterDateMessage = document.querySelector('#charter-date-message');
+
+var charterPlaceInput = document.querySelector('#charter-place');
+var charterPlaceMessage = document.querySelector('#charter-place-message');
+
+
+// deliveries and pick-ups input vars and output message vars 
+var deliveryDateInput = document.querySelector('#delivery-date');
+var deliveryDateMessage = document.querySelector('#delivery-date-message');
+
+var deliveryPlaceInput = document.querySelector('#delivery-place');
+var deliveryPlaceMessage = document.querySelector('#delivery-place-message');
+
+var pickupDateInput = document.querySelector('#pickup-date');
+var pickupDateMessage = document.querySelector('#pickup-date-message');
+
+var pickupPlaceInput = document.querySelector('#pickup-place');
+var pickupPlaceMessage = document.querySelector('#pickup-place-message');
+
+
+//customer contact details (customer group) vars for input & output msg 
+var firstNameInput = document.querySelector('#firstname');
+var firstNameMessage = document.querySelector('#firstname-message');
+
+var surNameInput = document.querySelector('#surname');
+var surNameMessage = document.querySelector('#surname-message');
+
+var emailInput = document.querySelector('#email');
+var emailMessage = document.querySelector('#email-message');
+
+var landlineInput = document.querySelector('#landline');
+var landlineMessage = document.querySelector('#landline-message');
+
+var mobileInput = document.querySelector('#mobile');
+var mobileMessage = document.querySelector('#mobile-message');
